09d3939aed92095c8894f04d68874ecdf3935e80
a10x <arthatixarca@gmail.com>
2022-07-31 22:37:48 +0100

    Initial Flutter project setup

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:recyclingapp/screens/test.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recycling App',
+      theme: _buildTheme(),
+      home: TestScreen()
+    );
+  }
+
+  ThemeData _buildTheme(){
+    var baseTheme = ThemeData(
+        primarySwatch: Colors.blue
+    );
+
+    return baseTheme.copyWith(
+      textTheme: GoogleFonts.jostTextTheme(baseTheme.textTheme)
+    );
+  }
+}
