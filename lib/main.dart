41b1e6245f485ffb5f03005c22414af0662569d7
tasnmb <tasnim.begom2@gmail.com>
2022-09-07 18:04:54 +0100

    Add Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,51 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+// NOTE: Currently navbar doesn't relocate to different screens, will fix in future
+class _MyAppState extends State<MyApp> {
+  int index = 0;
+  final pages = [
+    const Center(child: Text('test')),
+    const Center(child: Text('test2'))
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Recycling App',
-      theme: _buildTheme(),
-      home: const BarcodeScannerScreen()
-    );
+        title: 'Recycling App',
+        theme: _buildTheme(),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Recycling App',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          body: pages[index],
+          // NOTE: need to change font of navbar
+          bottomNavigationBar: NavigationBar(
+              height: 60,
+              selectedIndex: index,
+              onDestinationSelected: (index) =>
+                  setState(() => this.index = index),
+              destinations: const [
+                NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+                NavigationDestination(
+                    icon: Icon(Icons.document_scanner_outlined),
+                    label: 'Barcode Scanner'),
+              ]),
+        ));
   }
 
-  ThemeData _buildTheme(){
-    var baseTheme = ThemeData(
-        primarySwatch: Colors.blue
-    );
+  ThemeData _buildTheme() {
+    var baseTheme = ThemeData(primarySwatch: Colors.green);
 
     return baseTheme.copyWith(
-      textTheme: GoogleFonts.jostTextTheme(baseTheme.textTheme)
-    );
+        textTheme: GoogleFonts.jostTextTheme(baseTheme.textTheme));
   }
 }
