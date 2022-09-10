1671ed61f54c14b71abbe1676959493ddb6e528c
tasnmb <tasnim.begom2@gmail.com>
2022-09-10 12:59:19 +0100

    Add seperate files for screen widgets

---
+++ lib/screens/material_info.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Materials extends StatelessWidget {
+  const Materials({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(child: Text('Placeholder for Materials Screen'),)
+    );
+  }
+}
